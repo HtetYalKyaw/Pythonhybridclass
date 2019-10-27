c7e3f03b326429e649e04ebd13aece2297c7c88b
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2019-10-27 18:14:41 +0630

    Tax Calculator

---
+++ week 1/helloworld.py
@@ -0,0 +1,41 @@
+print("Hello World")
+
+#Sample
+Charge = 10000
+tax = 50/1000
+total_tax = Charge * tax
+total_paid = Charge + total_tax
+total_paid = ?
+
+#Problem
+
+Charge = 15000
+tax = 50/1000
+total_tax = Charge * tax
+total_tax
+750.0
+total_paid = Charge + total_tax 
+total_paid
+15750.0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
