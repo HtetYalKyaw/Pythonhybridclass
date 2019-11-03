756a58e421f35d89a1faa2962362348717283c38
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2019-11-03 17:42:28 +0630

    first commit

---
+++ Test.py
@@ -0,0 +1,28 @@
+# x =int (input ("Please entr an integer:"))
+
+# if  x < 5 :
+# 	x = 0
+# 	print ('x is smaller than 5')
+# elif x > 10:
+# 	print ('x is bigger than 10')
+# elif x == 7:
+# 	print('x is equal to 7')
+# elif x > 5 & x < 10:
+# 	print ('x is between 5 and 10')	
+
+# else:
+# 	print ('x is undefined')	
+
+# x = int (input("Please entr Ur age"))
+
+# if x > 1:
+# 	print('Go to school')
+
+# words = ['cat','window','defenestrate']
+# for w in words 
+# 	print (w,len(w))
+
+
+fruits =['apple','banana','cucumber','pineapple','orange']
+for w in words 
+	print (w,len(w))
---
+++ ifstatement.py
@@ -0,0 +1,12 @@
+x = int (input("Please entr an integer:"))
+
+if  x < 0:
+	x = 0
+	print('Negtive changed to zero')
+elif x == 0:
+	print('zero')
+elif x == 1:
+	print('Single')
+else:
+	print('More')			
+	
