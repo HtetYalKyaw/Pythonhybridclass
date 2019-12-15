850593bd748da0745fa3fd78e25bd8444aefe5e7
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2019-12-15 17:36:49 +0630

    student

---
+++ student/moduletest.py
@@ -0,0 +1,20 @@
+import mymodule
+
+a = mymodule.student["name"]
+
+x = mymodule.student["code"]
+
+b = mymodule.student1["father"]
+
+y = mymodule.student1["mother"]
+
+c = mymodule.student2["class"]
+
+z = mymodule.student2["age"]
+
+print(a)
+print(x)
+print(b)
+print(y)
+print(c)
+print(z)
