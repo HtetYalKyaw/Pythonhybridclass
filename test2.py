1a9dfec6ae21d7b3664d92f520a3ed2d0e04908b
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2020-01-19 17:37:57 +0630

    FileInputOutput

--- test2.py
+++ test2.py
@@ -1,37 +1,10 @@
-# def do_global():
-# 	global spam
-# 	spam = "global spam"
+class Dog():
 
-# spam = "test spam"
-# do_global()
-# print("After global assigment:", spam)
+	def__init__(self, breed):
+		self.breed = breed
+	def bark(self):
+		print(f'{self.breed} is barking.')
 
-
-def func():
-	nonlocal x
-	print('x is', x)
-
-	x = 5
-	print("Value of x is :", x)
-
-x = 10
-func()
-print("After nonlocal assigment:", spam)
-
-
-
-def scope_test():
-	def do_local():
-		spam = "local spam"
-
-	def do_nonlocal():
-		nonlocal spam
-		spam = "nonlocal spam"
-
-	spam = "test spam"
-	# do_local()
-	# print("After local assigment:", spam)
-	do_nonlocal()
-	print("After nonlocal assigment:", spam)
-
-scope_test()
+d = Dog('Labrador')
+d.bark()
+		
